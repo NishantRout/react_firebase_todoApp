7f1d9807e82a4a055c85f00221e5051b69541c86
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-17 11:30:24 +0530

    add todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    'Complete NPTEL Assignment 1',
+    'Computer Networks Project'
+  ]);
+  const [input, setInput] = useState('');
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  }
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      input
+    ]
+    );
+    setInput('');
+  }
+
   return (
     <div className="App">
+      <h1>Todo App</h1>
+      <input type="text" value={input} onChange={handleInput} />
+      <button onClick={addTodo}>Add Todo</button>
 
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li>{todo}</li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
