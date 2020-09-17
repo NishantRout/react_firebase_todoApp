b3e0e3bd3bb47ab2e73d2afec06369be4553c1fd
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-17 20:20:08 +0530

    edits-8:20pm

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import { Button, FormControl, makeStyles, TextField } from '@material-ui/core';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Todo from './components/Todo';
 import db from './config/firebase';
 import firebase from 'firebase';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '40ch',
+    },
+  },
+}));
+
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
 
+  const classes = useStyles();
+
   useEffect(() => {
     db.collection('todos').orderBy('timestamp', 'desc').onSnapshot((snapshot) => {
       setTodos(snapshot.docs.map((doc) => {
-        return (
-          doc.data().todo
-        )
+        return ({
+          id: doc.id,
+          todo: doc.data().todo,
+          timestamp: doc.data().timestamp.toDate()
+        })
       }))
     })
   }, []);
@@ -36,9 +49,8 @@ function App() {
     <div className="App">
       <h1>Todo App</h1>
       <form>
-        <FormControl>
-          <InputLabel>Enter Todo</InputLabel>
-          <Input type="text" value={input} onChange={handleInput} />
+        <FormControl className={classes.root}>
+          <TextField type="text" value={input} onChange={handleInput} id="outlined-basic" label="Enter Todo" variant="outlined" />
         </FormControl>
         <Button
           disabled={!input}
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/Todo.css';
-import { List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
+import { Button, FormControl, List, ListItem, ListItemAvatar, ListItemText, makeStyles, Modal, TextField } from '@material-ui/core';
 import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import db from '../config/firebase';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(5, 5, 5, 2)
+    },
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '40ch',
+        },
+    },
+}));
 
 function Todo(props) {
     const { todo } = props;
-    return (
-        < List >
-            <ListItemAvatar className="todo__list">
-                <ListItem>
-                    <ArrowRightAltIcon />
-                    <ListItemText className="todo__text" primary={todo} secondary="Dummy Deadline ⏰" />
-                </ListItem>
-            </ListItemAvatar>
+    const classes = useStyles();
+    const handleDelete = () => {
+        db.collection('todos').doc(todo.id).delete();
+    }
 
-        </List >
+    const [open, setOpen] = useState(false);
+    const [update, setUpdate] = useState(todo.todo);
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const handleUpdate = (e) => {
+        setUpdate(e.target.value);
+    }
+
+    const updateTodo = () => {
+        db.collection('todos').doc(todo.id).set({
+            todo: update
+        }, { merge: true })
+        setOpen(false);
+    }
+
+    return (
+        <>
+            <Modal
+                open={open}
+                onClose={handleClose}
+            >
+                <div className={classes.paper}>
+                    <FormControl className={classes.root}>
+                        <TextField type="text" value={update} onChange={handleUpdate} id="outlined-basic" label="Outlined" variant="outlined" />
+                    </FormControl>
+                    <Button onClick={updateTodo}>Update Todo</Button>
+                </div>
+            </Modal>
+            < List >
+                <ListItemAvatar className="todo__list">
+                    <ListItem>
+                        <ArrowRightAltIcon />
+                        <ListItemText className="todo__text" primary={todo.todo} secondary="Dummy Deadline ⏰" />
+                    </ListItem>
+                    <Button >
+                        <EditIcon onClick={handleOpen} />
+                    </Button>
+                    <Button >
+                        <DeleteIcon onClick={handleDelete} />
+                    </Button>
+                </ListItemAvatar>
+            </List >
+        </>
     )
 }
 
